de135ac839156bb03c9e6e62fa7d89c32dd2b248
marchino321 <39125194+marchino321@users.noreply.github.com>
2020-11-17 09:07:28 +0100

    Package different

---
+++ esempi/test.php
@@ -0,0 +1,16 @@
+<?php
+require 'vendor/autoload.php';
+// Creazione di istanze e definizione della posizione della cartella di caricamento del file
+$file = new MarcoUpload\MarcoUpload(__DIR__ . '/public_html');
+
+$file->upload($_FILES['archivio'], [
+	'move' => '/uploaded/',                 // Cartella di sestinazione
+	'size' => 2000000,                      // Grandezza file esempio 2MB
+	'type' => ['jpg', 'png']                // Estensioni accettate.
+]);
+
+if(!$file->getErros()){
+	echo 'Upload avvenuto con successo!';
+} else {
+	var_export($file->getErros());
+}
